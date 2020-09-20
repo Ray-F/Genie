2a5339167e01a929238bfd47b59ce50a3531599c
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2020-09-20 23:23:30 +1200

    update input controller array handle

--- controllers/InputController.js
+++ controllers/InputController.js
@@ -6,18 +6,18 @@ const saveChatbotInput = (req, res, next) => {
 
   const clientInput = {
     name: req.body.firstName + " " + req.body.lastName,
-    desc: req.body.customProperties.service_description,
-    date: req.body.customProperties.service_date,
-    location: req.body.customProperties.service_location,
+    desc: req.body.customProperties.service_description[0],
+    date: req.body.customProperties.service_date[0],
+    location: req.body.customProperties.service_location[0],
     terms: "",
     budgetEstimate: [0, 1000],
     quoted: 420,
     status: "pending",
     //extras
     email: req.body.email,
-    service_type: req.body.customProperties.service_type,
-    staff_count: req.body.customProperties.staff_count,
-    service_time: req.body.customProperties.service_time,
+    service_type: req.body.customProperties.service_type[0],
+    staff_count: req.body.customProperties.staff_count[0],
+    service_time: req.body.customProperties.service_time[0],
     create_date: req.body.createDate,
   };
 
