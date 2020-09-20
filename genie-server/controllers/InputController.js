316eab9ea5e31d05f235e76845044e1b953850d1
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2020-09-20 23:00:05 +1200

    bare bones testing

--- genie-server/controllers/InputController.js
+++ genie-server/controllers/InputController.js
@@ -1,16 +1,25 @@
-const saveChatbotInput = (req, res, next) => {
-  const fakeReq = {
-    body: {
-      email: 'johncena@gmail.com',
-      customProperties: {
-        service_type: ['Videography'],
-        staff_count: ['3'],
-        service_time: ['5'],
-      }
-    }
-  }
+const mongoClient = require("../models/mongoConnection");
+const mongo = require("mongodb");
 
-  res.json({ statusDescription: "Data acquired successfully!"});
+const saveChatbotInput = (req, res, next) => {
+  const clientCollection = mongoClient.db("teamregex").collection("clients");
+
+  const clientInput = {
+    email: req.body.email,
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    create_date: req.body.createDate,
+    service_description: req.body.customProperties.service_description,
+    service_type: req.body.customProperties.service_type,
+    staff_count: req.body.customProperties.staff_count,
+    service_time: req.body.customProperties.service_time,
+    service_location: req.body.customProperties.service_location,
+    service_date: req.body.customProperties.service_date,
+  };
+
+  clientCollection.insertOne(clientInput);
+
+  res.json({ statusDescription: "Data uploaded to db successfully!"});
 };
 
 module.exports = {
