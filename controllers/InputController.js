6c502677edc32965bd415e81ca32cd6788792f5c
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2020-09-20 23:16:06 +1200

    update inputController
    with db friendly wording

--- controllers/InputController.js
+++ controllers/InputController.js
@@ -5,16 +5,20 @@ const saveChatbotInput = (req, res, next) => {
   const clientCollection = mongoClient.db("teamregex").collection("clients");
 
   const clientInput = {
+    name: req.body.firstName + " " + req.body.lastName,
+    desc: req.body.customProperties.service_description,
+    date: req.body.customProperties.service_date,
+    location: req.body.customProperties.service_location,
+    terms: "",
+    budgetEstimate: [0, 1000],
+    quoted: 420,
+    status: "pending",
+    //extras
     email: req.body.email,
-    first_name: req.body.firstName,
-    last_name: req.body.lastName,
-    create_date: req.body.createDate,
-    service_description: req.body.customProperties.service_description,
     service_type: req.body.customProperties.service_type,
     staff_count: req.body.customProperties.staff_count,
     service_time: req.body.customProperties.service_time,
-    service_location: req.body.customProperties.service_location,
-    service_date: req.body.customProperties.service_date,
+    create_date: req.body.createDate,
   };
 
   clientCollection.insertOne(clientInput);
