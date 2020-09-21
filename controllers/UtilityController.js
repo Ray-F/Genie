c1cc010bf93fecbe1683b4a7bd2d6649892d984f
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2020-09-21 15:11:08 +1200

    Button toggle works and updates database

--- controllers/UtilityController.js
+++ controllers/UtilityController.js
@@ -1,4 +1,5 @@
 const mongoClient = require('../models/mongoConnection');
+const mongo = require("mongodb");
 const { getAllTerms } = require('../models/awsComprehendModel');
 
 
@@ -6,6 +7,20 @@ const utilityFunction = async (req, res, next) => {
   res.json({ statusCode: "Successfully made utility function call!"});
 }
 
+const updateStatus = async(req, res, next) => {
+  const clientsCollection = mongoClient.db("teamregex").collection("clients");
+
+  const response = {
+    status: req.body.status,
+  };
+
+  await clientsCollection.updateOne(
+    { _id: mongo.ObjectId(req.body.object_id) },
+    { $set: response }
+  );
+}
+
 module.exports = {
-  utilityFunction
+  utilityFunction,
+  updateStatus
 }
