ed46ee160c0bd75fb136f3f821a04f44859e5936
SERVER <rf.raymondfeng@gmail.com>
2020-09-23 08:52:47 +0000

    [Master] Fixed instabot formatting

--- controllers/InputController.js
+++ controllers/InputController.js
@@ -9,9 +9,9 @@ const saveChatbotInput = async (req, res, next) => {
 
   const clientInput = {
     name: req.body.firstName + " " + req.body.lastName,
-    desc: req.body.customProperties.service_description[0].substring(2, -2),
-    date: req.body.customProperties.service_date[0].substring(3, -3),
-    location: req.body.customProperties.service_location[0].substring(2, -2),
+    desc: req.body.customProperties.service_description.slice(2, -2),
+    date: req.body.customProperties.service_date.slice(3, -3),
+    location: req.body.customProperties.service_location.slice(2, -2),
     terms: "",
 
     //apply quoteCalculator.js functions here here
@@ -20,11 +20,15 @@ const saveChatbotInput = async (req, res, next) => {
 
     status: "pending",
     email: req.body.email,
-    service_type: req.body.customProperties.service_type[0].substring(2, -2),
-    staff_count: req.body.customProperties.staff_count[0].substring(2, -2),
-    service_time: req.body.customProperties.service_time[0].substring(2, -2),
+    service_type: req.body.customProperties.service_type.slice(2, -2),
+    staff_count: req.body.customProperties.staff_count.slice(2, -2),
+    service_time: req.body.customProperties.service_time.slice(2, -2),
     create_date: req.body.createDate,
   };
+
+  console.log(clientInput);
+  
+  console.log(req.body.customProperties);
 
   clientCollection.insertOne(clientInput);
 
