b5c0296dcd0f0305a506671f0b22bcdd3492cc00
Raymond Feng <rf.raymondfeng@gmail.com>
2020-09-23 22:49:59 +1200

    [Master] Added changes to dashboard

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -3,7 +3,8 @@ const mongoClient = require('../models/mongoConnection');
 
 const getClients = async (req, res, next) => {
   const clientCollection = mongoClient.db('teamregex').collection('clients');
-  const clients = await clientCollection.find({}).toArray();
+  const clients = await clientCollection.find({ archived: {$in: [false, null] }}).toArray();
+
 
   res.json(clients);
 }
