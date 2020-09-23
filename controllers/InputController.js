a77d7e72a9c3fc82c8fce58a50b08377237fa535
Raymond Feng <rf.raymondfeng@gmail.com>
2020-09-23 20:29:03 +1200

    [Master] Replaced slice with substring

--- controllers/InputController.js
+++ controllers/InputController.js
@@ -2,16 +2,16 @@ const mongoClient = require("../models/mongoConnection");
 const mongo = require("mongodb");
 const { getAllTerms } = require('../models/awsComprehendModel');
 
-// need to require quoteCalculator.js then apply the function 
+// need to require quoteCalculator.js then apply the function
 
 const saveChatbotInput = async (req, res, next) => {
   const clientCollection = mongoClient.db("teamregex").collection("clients");
 
   const clientInput = {
     name: req.body.firstName + " " + req.body.lastName,
-    desc: req.body.customProperties.service_description[0].slice(2, -2),
-    date: req.body.customProperties.service_date[0].slice(3, -3),
-    location: req.body.customProperties.service_location[0].slice(2, -2),
+    desc: req.body.customProperties.service_description[0].substring(2, -2),
+    date: req.body.customProperties.service_date[0].substring(3, -3),
+    location: req.body.customProperties.service_location[0].substring(2, -2),
     terms: "",
 
     //apply quoteCalculator.js functions here here
@@ -20,9 +20,9 @@ const saveChatbotInput = async (req, res, next) => {
 
     status: "pending",
     email: req.body.email,
-    service_type: req.body.customProperties.service_type[0].slice(2, -2),
-    staff_count: req.body.customProperties.staff_count[0].slice(2, -2),
-    service_time: req.body.customProperties.service_time[0].slice(2, -2),
+    service_type: req.body.customProperties.service_type[0].substring(2, -2),
+    staff_count: req.body.customProperties.staff_count[0].substring(2, -2),
+    service_time: req.body.customProperties.service_time[0].substring(2, -2),
     create_date: req.body.createDate,
   };
 
